12704fd65c09f27db1496c99f81549810b604e99
CultENOT <132144566+CultENOT@users.noreply.github.com>
2023-07-03 22:40:21 +0300

    Update calculation-clock.js

--- calculation-clock.js
+++ calculation-clock.js
@@ -5,18 +5,23 @@ function clock (hours, minutes) {
     minutes = prompt ("Сколько минут?", [5])
     if (hours > 24 || minutes > 60 || hours <= 0 || minutes <= -1) {
         return false
-    } else { 
-        if (hours > 12 ) {
-            let hoursNormal = hours - 12;
-            let minutesNormal = minutes / 5;
-            let res = Math.abs ((hoursNormal - minutesNormal) * 30)
-            return res 
-        }   else {
-            let minutesNormal = minutes / 5;
-            let res = Math.abs ((hours - minutesNormal) * 30)
-            return res 
+    } else {
+        if (hours == minutes) {
+            return alert('Разница 0 градусов!')
+        } else {
+            if (hours > 12 ) {
+                let hoursNormal = hours - 12;
+                let minutesNormal = minutes / 5;
+                let res = Math.abs ((hoursNormal - minutesNormal) * 30)
+                return res 
+            }   else {
+                let minutesNormal = minutes / 5;
+                let res = Math.abs ((hours - minutesNormal) * 30)
+                return res 
+            }
         }
     }
 }
 
 clock ()
+ 
